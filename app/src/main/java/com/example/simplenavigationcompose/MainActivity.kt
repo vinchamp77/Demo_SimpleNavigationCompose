34eb9b2d27f89a6d9fd91176538a6635c5882f32
vinchamp77 <vinchamp77@gmail.com>
2022-03-22 22:10:55 +1100

    Clean up imports

--- app/src/main/java/com/example/simplenavigationcompose/MainActivity.kt
+++ app/src/main/java/com/example/simplenavigationcompose/MainActivity.kt
@@ -3,11 +3,7 @@ package com.example.simplenavigationcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.example.simplenavigationcompose.navigation.BuildNavGraph
 import com.example.simplenavigationcompose.ui.theme.SimpleNavComposeAppTheme
