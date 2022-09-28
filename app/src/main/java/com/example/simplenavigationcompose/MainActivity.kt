3e2980019d7af9f36f8de5a535087f77f34863b9
vinchamp77 <vinchamp77@gmail.com>
2022-09-28 21:09:48 +1000

    paddingValues.calculateBottomPadding()

--- app/src/main/java/com/example/simplenavigationcompose/MainActivity.kt
+++ app/src/main/java/com/example/simplenavigationcompose/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.simplenavigationcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.example.simplenavigationcompose.ui.navigation.BottomBarNav
 import com.example.simplenavigationcompose.ui.navigation.NavGraph
@@ -27,8 +29,12 @@ private fun MainScreen() {
 
         Scaffold(
             bottomBar = { BottomBarNav(navController = navController) }
-        ) {
-            NavGraph(navController)
+        ) { paddingValues ->
+            NavGraph(
+                modifier = Modifier.padding(
+                    bottom = paddingValues.calculateBottomPadding()),
+                navController = navController
+            )
         }
     }
 }
